1e13820d52477b87a47720ebd13e5077c7abd9c4
shanmugapriyan98 <sravic23@asu.edu>
2024-07-16 19:42:45 -0500

    Added post method for adding recipes

--- src/app/addrecipes/page.tsx
+++ src/app/addrecipes/page.tsx
@@ -12,12 +12,27 @@
   }
   ```
 */
+'use client';
 import PhotoIcon from "@heroicons/react/24/solid/PhotoIcon"
+import { FormEvent } from "react"
 
 export default function Page() {
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+ 
+    const formData = new FormData(event.currentTarget)
+    const response = await fetch('http://localhost:8000/recipe/', {
+      method: 'POST',
+      body: formData,
+    })
+ 
+    // Handle response if necessary
+    const data = await response.json()
+    // ...
+  }
   return (
     <div className="px-6 py-6">
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="space-y-12">
           <div className="border-b border-gray-300 pb-12">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Add your recipe</h2>
@@ -25,19 +40,19 @@ export default function Page() {
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
               <div className="sm:col-span-4">
                 <label
-                  htmlFor="username"
+                  htmlFor="name"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
-                  Name of the Author
+                  Name of the Recipe
                 </label>
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                     <input
-                      id="username"
-                      name="username"
+                      id="name"
+                      name="name"
                       type="text"
-                      placeholder="Your Name Here"
-                      autoComplete="username"
+                      placeholder="Recipe Name Here"
+                      autoComplete="recipename"
                       className="block flex-1 border-0 bg-white py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -46,15 +61,15 @@ export default function Page() {
 
               <div className="col-span-full">
                 <label
-                  htmlFor="about"
+                  htmlFor="info"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Recipe Instructions
                 </label>
                 <div className="mt-2">
                   <textarea
-                    id="about"
-                    name="about"
+                    id="info"
+                    name="info"
                     rows={3}
                     className="block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     defaultValue={""}
@@ -114,7 +129,7 @@ export default function Page() {
             type="submit"
             className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Save
+            Submit
           </button>
         </div>
       </form>
--- src/app/viewrecipes/page.tsx
+++ src/app/viewrecipes/page.tsx
@@ -1,9 +1,48 @@
+const products = [
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"},
+    { id: 24, imageAlt: 'shshsh', name: "idly", color: "red", price: "23.09", href: "/", imageSrc: "chef.jpeg"}
+];
+
 export default function Page() {
     return(
-        <div className="bg-white dark:bg-gray-900 py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+        <div className="bg-white dark:bg-gray-900 py-8 lg:py-16 px-4 mx-auto w-full">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">View Recipes</h2>
-            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">This page contains all the available recipes</p>
-
+            <p className="mb-4 lg:mb-4 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">This page contains all the available recipes</p>
+            <div className="bg-white w-full">
+                <div className="mx-auto w-full px-4 py-16 sm:px-6 sm:py-24 lg:w-full lg:px-8">
+                    <div className="mt-4 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                        {products.map((product) => (
+                        <div key={product.id} className="group relative">
+                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                            <img
+                                alt={product.imageAlt}
+                                src={product.imageSrc}
+                                className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                            />
+                            </div>
+                            <div className="mt-4 flex justify-between">
+                            <div>
+                                <h3 className="text-sm text-gray-700">
+                                <a href={product.href}>
+                                    <span aria-hidden="true" className="absolute inset-0" />
+                                    {product.name}
+                                </a>
+                                </h3>
+                                <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+                            </div>
+                            <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                            </div>
+                        </div>
+                        ))}
+                    </div>
+                </div>
+          </div>
         </div>
     );
 }

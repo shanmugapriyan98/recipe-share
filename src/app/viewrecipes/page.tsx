dd233b44b26bc06bb2ad0085477b91d60a543d9a
shanmugapriyan98 <sravic23@asu.edu>
2024-07-19 11:44:51 -0500

    Refactored changes

--- src/app/viewrecipes/page.tsx
+++ src/app/viewrecipes/page.tsx
@@ -15,7 +15,7 @@ const Product = ({ product }: { product: Product }) => {
         <div key={product.id} className="group relative">
             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                 <img
-                    alt="chef's image"
+                    alt="chefs image"
                     src="/chef.jpeg"
                     className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
